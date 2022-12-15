0bc03df17a2c1cf293e72693fc97f5898d080fb9
heiwa4126 <heiwa4126@gmail.com>
2022-12-15 11:37:15 +0900

    basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
+import React, { useRef } from "react";
 import "./App.css";
 
 function App() {
+  const ref1 = useRef<HTMLInputElement>(null!);
+  const onClick1 = (msg: string) => {
+    ref1.current.value = msg;
+  };
+
   return (
-    <div>
+    <>
       <h1>Hello!</h1>
-    </div>
+      <div>
+        <FancyButton ref={ref1} value="test1">
+          <b>button</b>
+        </FancyButton>
+        <button onClick={() => onClick1("*1111*")}>test1</button>
+        <button onClick={() => onClick1("*2222*")}>test2</button>
+      </div>
+    </>
   );
 }
+
+interface FancyButtonProps {
+  value?: string;
+  children: JSX.Element | string;
+}
+
+const FancyButton = React.forwardRef<any, FancyButtonProps>((props, ref) => {
+  return (
+    <div>
+      <input type="text" ref={ref} value={props.value} />
+      <button>{props.children}</button>
+    </div>
+  );
+});
 
 export default App;
