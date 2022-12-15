4a85bbbe42a33311e5457e3494a08bda81f0b87f
heiwa4126 <heiwa4126@gmail.com>
2022-12-15 14:48:38 +0900

    add App3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React, { useRef } from "react";
 import "./App.css";
 import App1 from "./App1";
 import App2 from "./App2";
+import App3 from "./App3";
 
 function App() {
   return (
     <>
-      <App1 />
-      <App2 />
+      {/* <App1 />
+      <App2 /> */}
+      <App3 />
     </>
   );
 }
---
+++ src/App3.tsx
@@ -0,0 +1,70 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+import { Line } from "react-chartjs-2";
+import { randNumber } from "@ngneat/falso";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => randNumber({ min: -1000, max: 1000 })),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Dataset 2",
+      data: labels.map(() => randNumber({ min: -1000, max: 1000 })),
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
+function LineEx1() {
+  return <Line options={options} data={data} />;
+}
+
+function App3() {
+  return (
+    <div style={{ width: "32em", height: "16em" }}>
+      <LineEx1 />
+    </div>
+  );
+}
+
+export default App3;
